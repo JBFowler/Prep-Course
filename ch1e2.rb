9e1ccca6860db3746a0d4c81f29485b6f0de75eb
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-04-20 16:57:43 -0500

    Add first chapter files

---
+++ ch1e2.rb
@@ -0,0 +1,8 @@
+thousands = 1523 / 1000
+hundreds = 1500 % 1000 / 100
+tens = 1543 % 1000 % 100 / 10
+ones = 1543 % 1000 % 100 % 10
+puts thousands;
+puts hundreds;
+puts tens;
+puts ones;
---
+++ ch1e3.rb
@@ -0,0 +1,4 @@
+movies = {:gladiator => '2000', :Finding_Nemo => '2002', :Titans => '1999'}
+puts movies[:gladiator]
+puts movies[:Finding_Nemo]
+puts movies[:Titans]
